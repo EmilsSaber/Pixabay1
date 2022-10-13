ddacd646906c57259c78ad2b3a52bcfe5cf45bd0
Emil <emil.mametov.01@gmail.com>
2022-10-13 17:15:03 +0600

    pagination, scroll_pagination

--- app/src/main/java/kg/example/pixabay/MainActivity.kt
+++ app/src/main/java/kg/example/pixabay/MainActivity.kt
@@ -3,16 +3,20 @@ package kg.example.pixabay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import kg.example.pixabay.adapter.ImageAdapter
 import kg.example.pixabay.app.App
 import kg.example.pixabay.databinding.ActivityMainBinding
+import kg.example.pixabay.model.ImageModel
 import kg.example.pixabay.model.PixModel
+import okhttp3.internal.notify
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    var imageAdapter = ImageAdapter(mutableListOf())
+    var imageAdapter = ImageAdapter(arrayListOf())
     var page = 1
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +35,18 @@ class MainActivity : AppCompatActivity() {
         binding.btnClick.setOnClickListener {
             doRequest()
         }
+
+        binding.recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(1)) {
+                    doRequest()
+                    ++page
+
+                }
+            }
+        })
+
     }
 
     private fun doRequest() {
@@ -38,7 +54,10 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<PixModel>, response: Response<PixModel>) {
                 if (response.isSuccessful) {
                     Log.e("ololo", "onResponse: ${response.body()?.hits!![0].largeImageURL}")
-                    imageAdapter = ImageAdapter(response.body()?.hits!!)
+                    val list  = arrayListOf<ImageModel>()
+                    response.body()?.hits?.forEach{
+                        imageAdapter.addImage(it)
+                    }
                     binding.recycler.adapter = imageAdapter
                 }
             }
--- app/src/main/java/kg/example/pixabay/adapter/ImageAdapter.kt
+++ app/src/main/java/kg/example/pixabay/adapter/ImageAdapter.kt
@@ -7,12 +7,8 @@ import coil.load
 import kg.example.pixabay.databinding.ItemPixabayBinding
 import kg.example.pixabay.model.ImageModel
 
-class ImageAdapter(private val list: List<ImageModel>):RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
-    class ImageViewHolder(private val binding: ItemPixabayBinding):RecyclerView.ViewHolder(binding.root) {
-        fun onBind(imageModel: ImageModel) {
-            binding.imageView.load(imageModel.largeImageURL)
-        }
-    }
+class ImageAdapter(private val list: ArrayList<ImageModel>):RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         return ImageViewHolder(ItemPixabayBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -21,8 +17,16 @@ class ImageAdapter(private val list: List<ImageModel>):RecyclerView.Adapter<Imag
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
         holder.onBind(list[position])
     }
+    fun addImage(imageModel: ImageModel){
+        list.add(imageModel)
+    }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+    class ImageViewHolder(private val binding: ItemPixabayBinding):RecyclerView.ViewHolder(binding.root) {
+        fun onBind(imageModel: ImageModel) {
+            binding.imageView.load(imageModel.largeImageURL)
+        }
     }
 }
